4a0543152bc6bca609b7c356216c7b7fae9420ed
Zohreh Habibi <your-email@example.com>
2026-04-23 22:35:46 +0200

    feat: add canvas initialization function and structured draw call

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-function draw() {
+function init() {
   const canvas = document.getElementById("game-canvas");
   const ctx = canvas.getContext("2d");
+  draw(ctx);
+}
+init();
+
+
+function draw(ctx) {
   ctx.fillStyle = "rgb(200 0 0)";
   ctx.fillRect(10, 10, 50, 50);
 
   ctx.fillStyle = "rgb(0 0 200 / 50%)";
   ctx.fillRect(30, 30, 50, 50);
 }
-draw();
