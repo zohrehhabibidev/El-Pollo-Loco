304597a51e48061494b7910926c377a1a48ab658
Zohreh Habibi <your-email@example.com>
2026-04-24 11:10:28 +0200

    feat: render character image after load using structured init and draw functions

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
+let canvas;
+let ctx;
+let img = new Image();
+
 function init() {
-  const canvas = document.getElementById("game-canvas");
-  const ctx = canvas.getContext("2d");
+  canvas = document.getElementById("game-canvas");
+  ctx = canvas.getContext("2d");
 
-  const img = new Image();
   img.src = "assets/img/character/1_idle/idle/I-1.png";
-
-  img.onload = function () {
-
-    ctx.drawImage(img, 50, 50, 100, 200);
-  };
 }
+
+function draw() {
+  ctx.drawImage(img, 50, 50, 200, 300);
+}
+
+img.onload = function () {
+  draw();
+};
 
 init();
