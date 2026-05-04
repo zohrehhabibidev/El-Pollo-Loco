c26dc15a487f15c48a3c7d44470e7092551753cf
Zohreh Habibi <your-email@example.com>
2026-05-04 18:22:16 +0200

    feat(endboss): add Endboss class

---
+++ js/classes/endboss.class.js
@@ -0,0 +1,28 @@
+const endbossWalkImages = [
+  "assets/img/enemies/boss_chicken/1_walk/G1.png",
+  "assets/img/enemies/boss_chicken/1_walk/G2.png",
+  "assets/img/enemies/boss_chicken/1_walk/G3.png",
+  "assets/img/enemies/boss_chicken/1_walk/G4.png",
+];
+
+/**
+ * Represents the endboss enemy at the end of the level.
+ *
+ * @extends MovableObject
+ */
+class Endboss extends MovableObject {
+  /**
+   * Creates the endboss with its start position and size.
+   */
+  constructor() {
+    super();
+
+    this.loadImages(endbossWalkImages);
+    this.img = this.imageCache[endbossWalkImages[0]];
+
+    this.x = 2100;
+    this.y = 120;
+    this.width = 260;
+    this.height = 330;
+  }
+}
